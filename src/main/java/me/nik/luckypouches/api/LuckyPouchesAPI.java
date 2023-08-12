a66867e0d1d70ff6d0befa4997cbe62f83aefe58
Nik <61439310+NikV2@users.noreply.github.com>
2023-08-12 21:10:17 +0300

    Update LuckyPouchesAPI.java

--- src/main/java/me/nik/luckypouches/api/LuckyPouchesAPI.java
+++ src/main/java/me/nik/luckypouches/api/LuckyPouchesAPI.java
@@ -7,9 +7,27 @@ import org.bukkit.entity.Player;
 
 public interface LuckyPouchesAPI {
 
+    /**
+     * Register an animation
+     * 
+     * @param animation The animation
+     */
     void registerAnimation(AnimationType... animation);
 
+    /**
+     * Register a currency
+     * 
+     * @param currency The currency
+     */
     void registerCurrency(CurrencyType... currency);
 
+    /**
+     * Give the reward to a player
+     * This is designed to be used once inside animations
+     * 
+     * @param player The player
+     * @param pouch The pouch
+     * @return The rewarded amount
+     */
     long giveReward(Player player, Pouch pouch);
 }
